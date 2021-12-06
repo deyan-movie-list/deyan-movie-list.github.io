fcba1da03e42180fd049d2fdde9244a76afbd71b
deyan petrov <deyanp19@hotmail.com>
2021-12-06 14:23:40 +0200

    update url

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'https://dashboard.heroku.com';
+export const baseUrl = 'https://movie-list-deyan.herokuapp.com';
 
 export const login = `${baseUrl}/users/login`;
 export const logout  =`${baseUrl}/users/logout`;
